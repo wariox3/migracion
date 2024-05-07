6b0ecde27c2b1b2b84cfcdcb6366005762bb46d2
MarioA <mestradaz3@gmail.com>
2024-05-07 13:26:27 -0500

    varios

--- eliminar_archivos_semantica.py
+++ eliminar_archivos_semantica.py
@@ -113,7 +113,7 @@ def eliminar_anio(anio):
 #limpiar_errores_identificacion()
 #limpiar_errores_singuia()
 #SELECT YEAR(g.fecha_ingreso) AS año, COUNT(*) AS total_ingresos FROM doc_masivo d LEFT JOIN tte_guia g ON d.identificador = g.codigo_guia_pk WHERE d.backup = false AND d.codigo_masivo_tipo_fk = 'TteGuia'GROUP BY YEAR(g.fecha_ingreso)
-eliminar_anio(2020)
+eliminar_anio(2021)
 
 cursorMysql.close()
 conexion.close()
